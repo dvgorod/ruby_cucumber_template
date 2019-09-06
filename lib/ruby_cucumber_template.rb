54a3fcf7d72fc9ae0372de85f50d0a577f7bf6dd
Dmitriy Gorodenkov <dgorodenkov@alfabank.ru>
2019-09-06 21:43:01 +0300

    first commit

---
+++ lib/ruby_cucumber_template.rb
@@ -0,0 +1,6 @@
+require "ruby_cucumber_template/version"
+
+module RubyCucumberTemplate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_cucumber_template/version.rb
@@ -0,0 +1,3 @@
+module RubyCucumberTemplate
+  VERSION = "0.1.0"
+end
